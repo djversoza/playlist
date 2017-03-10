ccb48606c4eca034bde1cddea77e4770e2f9496b
DJ Versoza <djversoza@gmail.com>
2017-03-10 09:04:01 -0700

    fixed a bug with the the choose track button

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $.ajax({
     url: 'https://lit-fortress-6467.herokuapp.com/object',
   }).done(function(data) {
     var arr = data.results;
-
     for (i = 0; i < 3; i++) {
       var rand = arr[Math.floor(Math.random() * arr.length)]
       console.log(rand.cover_art)
